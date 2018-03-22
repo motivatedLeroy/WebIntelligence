db54e8abc5dda593efe08b3705c9b11471c9c040
Viktor Puselja <viktor.puselja@gmail.com>
2018-03-22 13:13:29 +0100

    Add basic prediction example

---
+++ predict.py
@@ -0,0 +1,17 @@
+from surprise import Dataset
+from surprise import Reader
+from surprise import SVD
+from surprise import accuracy
+from surprise.model_selection import KFold
+
+dataset_file = 'dataset.txt'
+
+reader = Reader(line_format='user item rating', sep='\t')
+data = Dataset.load_from_file(dataset_file, reader=reader)
+algo = SVD()
+kf = KFold(n_splits=10)
+
+for trainset, testset in kf.split(data):
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+    accuracy.rmse(predictions, verbose=True)
---
+++ preprocess.py
@@ -0,0 +1,19 @@
+import json
+
+output_fname = 'dataset.txt'
+
+input_file = 'one_week/20170102'
+
+print('>>> Start reading file...')
+with open(output_fname, 'w') as f:
+    count = 0
+    for line in open(input_file):
+        obj = json.loads(line.strip())
+        try:
+            uid, iid, activeTime = obj['userId'], obj['id'], str(obj['activeTime'])        except Exception as e:
+            continue
+        print('\t'.join([uid, iid, activeTime]), file=f)
+        count += 1
+        #if count >= 10000:
+        #    break
+print('>>> Done!')
