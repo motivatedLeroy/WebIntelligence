38cbe55b4310f2d124b983c2a48527f2663f5c61
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-06 00:15:17 +0200

    Rewrite preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
+
 import json
 
 input_file = 'one_week/20170102'
-output_fname = 'dataset.txt'
+output_file = 'dataset.txt'
 
-with open(output_fname, 'w') as f:
-    for line in open(input_file):
+with open(input_file) as fin, open(output_file, 'w') as fout:
+    for line in fin:
         obj = json.loads(line.strip())
-        try:
-            uid, iid, activeTime = obj['userId'], obj['id'], obj['activeTime']
-        except Exception as e:
-            continue
-        print('\t'.join([uid, iid, str(activeTime)]), file=f)
+        is_news_article = 'id' in obj
+        if not is_news_article:
+            continue # no usable information
+
+        uid, iid = obj['userId'], obj['id']
+        subscribed = 'pluss' in obj['url']
+        if not subscribed:
+            continue # TODO: extract sessions for unsubscribed users
+
+        active_time_recorded = 'activeTime' in obj
+        if not active_time_recorded:
+            continue # TODO: consider default active time
+
+        active_time = obj['activeTime']
+        print('\t'.join([uid, iid, str(active_time)]), file=fout)
+
+        # TODO: print progress
