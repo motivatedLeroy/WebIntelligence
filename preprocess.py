d383b9a8a0b7bf68dfe41a1d29e9cc3903335bae
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-05 06:29:39 +0200

    Clean up preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,13 @@
 import json
 
+input_file = 'one_week/20170102'
 output_fname = 'dataset.txt'
 
-input_file = 'one_week/20170102'
-
-print('>>> Start reading file...')
 with open(output_fname, 'w') as f:
-    count = 0
     for line in open(input_file):
         obj = json.loads(line.strip())
         try:
-            uid, iid, activeTime = obj['userId'], obj['id'], str(obj['activeTime'])
+            uid, iid, activeTime = obj['userId'], obj['id'], obj['activeTime']
         except Exception as e:
             continue
-        print('\t'.join([uid, iid, activeTime]), file=f)
-        count += 1
-        #if count >= 10000:
-        #    break
-print('>>> Done!')
+        print('\t'.join([uid, iid, str(activeTime)]), file=f)
