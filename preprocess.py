b1ca1926658aeeb6644d7cbe3ffc79a3ec026ef8
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-06 00:45:35 +0200

    Implement preprocessing for content-based filtering

--- preprocess.py
+++ preprocess.py
@@ -4,8 +4,10 @@ import json
 
 raw_dataset = 'one_week/20170102'
 collaborative = 'collaborative.csv'
+content = 'content.csv'
 
-with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll:
+with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
+        content, 'w') as fcont:
     for line in fin:
         obj = json.loads(line.strip())
         is_news_article = 'id' in obj
@@ -18,10 +20,13 @@ with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll:
             continue # TODO: extract sessions for unsubscribed users
 
         active_time_recorded = 'activeTime' in obj
-        if not active_time_recorded:
-            continue # TODO: consider default active time
+        if active_time_recorded: # TODO: consider default active time
+            active_time = obj['activeTime']
+            print('\t'.join([uid, iid, str(active_time)]), file=fcoll)
 
-        active_time = obj['activeTime']
-        print('\t'.join([uid, iid, str(active_time)]), file=fcoll)
+        keywords_avaiable = 'keywords' in obj
+        if keywords_avaiable:
+            keywords = obj['keywords']
+            print('\t'.join([uid, iid, keywords]), file=fcont)
 
         # TODO: print progress
