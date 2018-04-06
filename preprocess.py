4265322d9ad34eb7f7f87d91b3bac142f9bd53b1
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-06 19:39:32 +0200

    Refactor parsing a line in preprocessing into a function

--- preprocess.py
+++ preprocess.py
@@ -9,6 +9,46 @@ content = 'content.csv'
 
 max_print_count = int(argv[1]) if len(argv) > 1 else None
 
+def parse_line(line, sessions_count, sessions):
+    obj = json.loads(line.strip())
+
+    is_news_article = 'id' in obj
+    if not is_news_article:
+        return None
+
+    iid = obj['userId']
+    event = { 'iid': iid }
+
+    uid, eid = obj['userId'], obj['eventId']
+    is_subscribed = 'pluss' in obj['url']
+    if is_subscribed:
+        event['uid'] = uid
+    else:
+        start, stop = obj['sessionStart'], obj['sessionStop']
+        if not start and uid not in sessions:
+            #print('warning: session not initialized [eid:{}]'.format(eid))
+            start = True
+
+        if start:
+            sessions_count[uid] = sessions_count.get(uid, 0) + 1
+            sid = uid + '#' + str(sessions_count[uid])
+            #if uid in sessions:
+            #    print('warning: session not terminated [eid:{}, c:{}]'.
+            #          format(eid, sessions_count[uid] - 1))
+            sessions[uid] = sid
+        else:
+            sid = sessions[uid]
+
+        if stop:
+            del sessions[uid]
+
+        event['uid'] = sid
+
+    event['active_time'] = obj['activeTime'] if 'activeTime' in obj else None
+    event['keywords'] = obj['keywords'] if 'keywords' in obj else None
+
+    return event
+
 with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
         content, 'w') as fcont:
     read_count = 0
@@ -21,54 +61,29 @@ with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
     print('user\titem\trating', file=fcoll)
     print('user\titem\tcontent', file=fcont)
     for line in fin:
-        read_count += 1
-        print('preprocessing: {} line(s) read, {}@coll, {}@cont written.'.
-              format(read_count, print_coll_count, print_cont_count), end='\r')
-
         if (max_print_count is not None
             and print_coll_count == print_cont_count == max_print_count):
             break
 
-        obj = json.loads(line.strip())
-        is_news_article = 'id' in obj
-        if not is_news_article:
-            continue # no usable information
+        event = parse_line(line, sessions_count, sessions)
+        if event is not None:
+            iid, uid = event['iid'], event['uid']
+            active_time, keywords = event['active_time'], event['keywords']
 
-        uid, iid, eid = obj['userId'], obj['id'], obj['eventId']
-        subscribed = 'pluss' in obj['url']
-        if not subscribed:
-            start, stop = obj['sessionStart'], obj['sessionStop']
-            if not start and uid not in sessions:
-                #print('warning: session not initialized [eid:{}]'.format(eid))
-                start = True
-
-            if start:
-                sessions_count[uid] = sessions_count.get(uid, 0) + 1
-                sid = uid + '#' + str(sessions_count[uid])
-                #if uid in sessions:
-                #    print('warning: session not terminated [eid:{}, c:{}]'.
-                #          format(eid, sessions_count[uid] - 1))
-                sessions[uid] = sid
-            else:
-                sid = sessions[uid]
-
-            if stop:
-                del sessions[uid]
-
-            uid = sid
-
-        if max_print_count is None or print_coll_count < max_print_count:
-            active_time_recorded = 'activeTime' in obj
-            if active_time_recorded: # TODO: consider default active time
-                active_time = obj['activeTime']
+            if (active_time is not None
+                and (max_print_count is None
+                     or print_coll_count < max_print_count)):
                 print('\t'.join([uid, iid, str(active_time)]), file=fcoll)
                 print_coll_count += 1
 
-        if max_print_count is None or print_cont_count < max_print_count:
-            keywords_avaiable = 'keywords' in obj
-            if keywords_avaiable:
-                keywords = obj['keywords']
+            if (keywords is not None
+                and (max_print_count is None
+                     or print_cont_count < max_print_count)):
                 print('\t'.join([uid, iid, keywords]), file=fcont)
                 print_cont_count += 1
+
+        read_count += 1
+        print('preprocessing: {} line(s) read, {}@coll, {}@cont written.'.
+              format(read_count, print_coll_count, print_cont_count), end='\r')
 
     print()
