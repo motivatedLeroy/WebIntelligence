427bd3356ab4699b7cab9a9072772064da554a73
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-06 14:33:13 +0200

    Rename count to read_count

--- preprocess.py
+++ preprocess.py
@@ -8,7 +8,7 @@ content = 'content.csv'
 
 with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
         content, 'w') as fcont:
-    count = 0
+    read_count = 0
     sessions_count = {}
     sessions = {}
 
@@ -16,8 +16,8 @@ with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
     print('user\titem\trating', file=fcoll)
     print('user\titem\tcontent', file=fcont)
     for line in fin:
-        count += 1
-        print('preprocessing: {} line(s) read.'.format(count), end='\r')
+        read_count += 1
+        print('preprocessing: {} line(s) read.'.format(read_count), end='\r')
 
         obj = json.loads(line.strip())
         is_news_article = 'id' in obj
