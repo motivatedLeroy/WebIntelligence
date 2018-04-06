e7d0d5d92f0af6061a96716f14d50f5086ef4e89
Viktor Puselja <viktor.puselja@gmail.com>
2018-04-06 14:52:28 +0200

    Implement output limit option for preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import json
+from sys import argv
 
 raw_dataset = 'one_week/20170102'
 collaborative = 'collaborative.csv'
 content = 'content.csv'
 
+max_print_count = int(argv[1]) if len(argv) > 1 else None
+
 with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
         content, 'w') as fcont:
     read_count = 0
+    print_coll_count = 0
+    print_cont_count = 0
     sessions_count = {}
     sessions = {}
 
@@ -17,7 +22,12 @@ with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
     print('user\titem\tcontent', file=fcont)
     for line in fin:
         read_count += 1
-        print('preprocessing: {} line(s) read.'.format(read_count), end='\r')
+        print('preprocessing: {} line(s) read, {}@coll, {}@cont written.'.
+              format(read_count, print_coll_count, print_cont_count), end='\r')
+
+        if (max_print_count is not None
+            and print_coll_count == print_cont_count == max_print_count):
+            break
 
         obj = json.loads(line.strip())
         is_news_article = 'id' in obj
@@ -47,14 +57,18 @@ with open(raw_dataset) as fin, open(collaborative, 'w') as fcoll, open(
 
             uid = sid
 
-        active_time_recorded = 'activeTime' in obj
-        if active_time_recorded: # TODO: consider default active time
-            active_time = obj['activeTime']
-            print('\t'.join([uid, iid, str(active_time)]), file=fcoll)
+        if max_print_count is None or print_coll_count < max_print_count:
+            active_time_recorded = 'activeTime' in obj
+            if active_time_recorded: # TODO: consider default active time
+                active_time = obj['activeTime']
+                print('\t'.join([uid, iid, str(active_time)]), file=fcoll)
+                print_coll_count += 1
 
-        keywords_avaiable = 'keywords' in obj
-        if keywords_avaiable:
-            keywords = obj['keywords']
-            print('\t'.join([uid, iid, keywords]), file=fcont)
+        if max_print_count is None or print_cont_count < max_print_count:
+            keywords_avaiable = 'keywords' in obj
+            if keywords_avaiable:
+                keywords = obj['keywords']
+                print('\t'.join([uid, iid, keywords]), file=fcont)
+                print_cont_count += 1
 
     print()
